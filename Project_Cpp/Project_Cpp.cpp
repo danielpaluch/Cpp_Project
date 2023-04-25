4302bb609f0f4ddd33423426be03771fa8266599
danielpaluch <danielpaluch37@gmail.com>
2023-04-25 20:40:10 +0200

    sqlite3 database connection done

---
+++ Project_Cpp/Project_Cpp.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include "sqlite/sqlite3.h"
+
+using namespace std;
+
+
+void menu() {
+    int option;
+    cout << "1. Zaloguj sie \n";
+    cout << "2. Zarejestruj sie \n";
+    cout << "3. Wyjdz \n";
+
+    cout << "Wybierz opcje: ";
+    cin >> option;
+}
+
+
+int main()
+{
+    sqlite3 *db;
+    int rc = sqlite3_open("test.db", &db);
+    if (rc) {
+        cout << "Brak polaczenia z baza, zamykam program.";
+        sqlite3_close(db);
+    }
+    menu();
+    sqlite3_close(db);
+}
