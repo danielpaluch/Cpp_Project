2c16cd3b01de9a56311d1dd3bc3b0607de06ef7f
danielpaluch <danielpaluch37@gmail.com>
2023-05-01 13:07:57 +0200

    register done, bug fixes, login and menu of logged user started

--- Project_Cpp/Project_Cpp.cpp
+++ Project_Cpp/Project_Cpp.cpp
@@ -1,89 +1,212 @@
-﻿#include <iostream>
-#include <stdio.h>
-#include "sqlite/sqlite3.h"
+﻿#include "sqlite/sqlite3.h"
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-void logIn() {
-    cout << "LOGOWANIE";
-}
-void signUp() {
-    system("CLS");
-    string login, pswd;
-    cout << "Podaj login: ";
-    cin >> login;
-    cout << "Podaj haslo: ";
-    cin >> pswd;
-    cout << "PODANO: " << login << ", " << pswd;
-}
+class User {
+public:
+    int id = -1;
+    string login = "";
+};
+User user;
 
-void menu() {
+void menu(sqlite3* db);
+void loggedUserMenu(sqlite3* db) {
     int option;
-    cout << "1. Zaloguj sie \n";
-    cout << "2. Zarejestruj sie \n";
-    cout << "3. Wyjdz \n";
+    cout << "Witaj " + user.login + ", co chcesz zrobic?";
+    cout << "1. Wyswietl witryny \n";
+    cout << "2. Dodaj witryne \n";
+    cout << "3. Wyloguj \n";
 
     cout << "Wybierz opcje: ";
     cin >> option;
 
     switch (option) {
-        case 1: {
-            system("CLS");
-            logIn();
-        }
+    case 1: { // Wyswietl witryny
+        system("CLS");
+        logIn(db);
+    }
+          break;
+    case 2: { // Dodaj witryne
+        system("CLS");
+        signUp(db);
+    }
+          break;
+    case 3: system("exit"); // Wyloguj
         break;
-        case 2: {
-            system("CLS");
-            signUp();
+    default: {
+        system("CLS");
+        cout << "Nie ma takiej opcji, sprobuj ponownie.. \n";
+        menu(db);
+    }
+    }
+
+};
+void logIn(sqlite3* db) {
+    string login, pswd;
+    sqlite3_stmt* stmt;
+    cout << "Login: ";
+    cin >> login;
+    cout << "Hasło: ";
+    cin >> pswd;
+
+    string sql_query = "SELECT * FROM users WHERE (name = ? AND password = ?)";
+    sqlite3_prepare_v2(db, sql_query.c_str(), -1, &stmt, nullptr);
+    sqlite3_bind_text(stmt, 1, login.c_str(), -1, SQLITE_TRANSIENT);
+    sqlite3_bind_text(stmt, 2, pswd.c_str(), -1, SQLITE_TRANSIENT);
+
+    int result_code = sqlite3_step(stmt);
+    if (result_code == SQLITE_ROW) { // ZNALEZIONO
+        int id = sqlite3_column_int(stmt, 0);
+        const char* name = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1));
+        user.id = id;
+        user.login = name; // "zalogowanie uzytkownika";
+        sqlite3_finalize(stmt);
+        loggedUserMenu(db);
+    }
+    else {
+        cout << "Bledne dane, sprobuj jeszcze raz: ";
+        logIn(db);
+    }
+}
+void signUp(sqlite3* db) {
+    sqlite3_stmt* stmt;
+    char* err_msg;
+    string login, pswd;
+    cout << "Podaj login: ";
+    cin >> login;
+    cout << "Podaj haslo: ";
+    cin >> pswd;
+
+    if (login == "" || pswd == "") { //50znakow
+        cout << "Niepoprawne hasło lub login.. \n";
+        menu(db);
+    }
+    string sql_query = "SELECT * FROM users WHERE name = ?";
+    sqlite3_prepare_v2(db, sql_query.c_str(), -1, &stmt, nullptr);
+    sqlite3_bind_text(stmt, 1, login.c_str(), -1, SQLITE_TRANSIENT);
+
+    int result_code = sqlite3_step(stmt);
+    sqlite3_finalize(stmt);
+
+    if (result_code != SQLITE_ROW && result_code == SQLITE_DONE) {
+        // brak uzytkownika o takim loginie
+        sql_query = "INSERT INTO users (name, password) VALUES ('" + login + "', '" + pswd + "')";
+        int rc = sqlite3_exec(db, sql_query.c_str(), nullptr, nullptr, &err_msg);
+        cout << rc;
+        if (rc != SQLITE_OK) {
+            cout << "Blad podczas dodawania: " << err_msg << endl;
+            sqlite3_free(err_msg);
+            sqlite3_close(db);
         }
-        break;
-        case 3: system("exit");
-        break;
-        default: {
-            system("CLS");
-            cout << "Nie ma takiej opcji, sprobuj ponownie.. \n";
-            menu();
-        }
+        system("CLS");
+        cout << "Pomyslnie zarejestrowano! \n";
+        menu(db);
+    }
+    else {
+        system("CLS");
+        cout << "Taki użytkownik już istnieje, sprobuj ponownie \n";
+        signUp(db);
     }
 }
 
+void menu(sqlite3* db) {
+    int option;
+    cout << "1. Zaloguj sie \n";
+    cout << "2. Zarejestruj sie \n";
+    cout << "3. Wyjdz \n";
 
-int main()
-{
-    sqlite3 *db;
-    char* messageError;
+    string sql_query = "SELECT * FROM users";
+    sqlite3_stmt* stmt;
+    int rc = sqlite3_prepare_v2(db, sql_query.c_str(), -1, &stmt, nullptr);
+    if (rc != SQLITE_OK) {
+        cout << "Error preparing SELECT statement: " << sqlite3_errmsg(db) << endl;
+        sqlite3_close(db);
+    }
+
+    while (sqlite3_step(stmt) == SQLITE_ROW) {
+        int id = sqlite3_column_int(stmt, 0);
+        const char* name = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1));
+        const char* pswd = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 2));
+        cout << "ID: " << id << ", LOGIN: " << name << ", PASSWORD: " << pswd << endl;
+    }
+    sqlite3_finalize(stmt);
+
+    cout << "Wybierz opcje: ";
+    cin >> option;
+
+    switch (option) {
+    case 1: {
+        system("CLS");
+        logIn(db);
+    }
+          break;
+    case 2: {
+        system("CLS");
+        signUp(db);
+    }
+          break;
+    case 3: system("exit");
+        break;
+    default: {
+        system("CLS");
+        cout << "Nie ma takiej opcji, sprobuj ponownie.. \n";
+        menu(db);
+    }
+    }
+}
+
+int main() {
+    sqlite3* db;
+    char* err_msg = nullptr;
     int rc = sqlite3_open("test.db", &db);
 
     if (rc) {
-        cout << "Brak polaczenia z baza, zamykam program.";
+        cout << "Blad podczas polaczenia z baza danych: " << sqlite3_errmsg(db) << endl;
         sqlite3_close(db);
-        system("EXIT");
+        return 1;
     }
-    string sqlUserInit = "CREATE TABLE IF NOT EXISTS USER("
-        "USER_ID INT PRIMARY KEY NOT NULL, "
-        "NAME CHAR(50) NOT NULL, "
-        "PASSWORD CHAR(50) NOT NULL);";
-    rc = sqlite3_exec(db, sqlUserInit.c_str(), NULL, 0, &messageError);
+    string sql_query = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, password TEXT NOT NULL)";
+    rc = sqlite3_exec(db, sql_query.c_str(), nullptr, nullptr, &err_msg);
 
     if (rc != SQLITE_OK) {
-        cout << "Error with table USER: " << messageError;
-        sqlite3_free(messageError);
+        cout << "Tworzenie tabeli: " << err_msg << endl;
+        sqlite3_free(err_msg);
+        sqlite3_close(db);
+        return 1;
     }
-
-    string sqlSiteInit = "CREATE TABLE IF NOT EXISTS SITE("
-        "SITE_ID INT PRIMARY KEY NOT NULL, "
-        "NAME CHAR(50) NOT NULL, "
-        "LOGIN CHAR(50) NOT NULL, "
-        "PASSWORD CHAR(50) NOT NULL, "
-        "USER_ID INT NOT NULL, "
-        "FOREIGN KEY(USER_ID) REFERENCES USER(USER_ID));";
-    rc = sqlite3_exec(db, sqlSiteInit.c_str(), NULL, 0, &messageError);
-
+    /* DODAWANIE
+    sql_query = "INSERT INTO users (name, age) VALUES ('John', 30)";
+    rc = sqlite3_exec(db, sql_query.c_str(), nullptr, nullptr, &err_msg);
     if (rc != SQLITE_OK) {
-        cout << "Error with table SITE: " << messageError;
-        sqlite3_free(messageError);
+        std::cerr << "Error inserting record: " << err_msg << std::endl;
+        sqlite3_free(err_msg);
+        sqlite3_close(db);
+        return 1;
+    }
+    */
+    menu(db);
+    /*
+    sql_query = "SELECT * FROM users";
+    sqlite3_stmt* stmt;
+    rc = sqlite3_prepare_v2(db, sql_query.c_str(), -1, &stmt, nullptr);
+    cout << rc;
+    if (rc != SQLITE_OK) {
+        cout << "Error preparing SELECT statement: " << sqlite3_errmsg(db) << endl;
+        sqlite3_close(db);
+        return 1;
     }
 
-    menu();
+    while (sqlite3_step(stmt) == SQLITE_ROW) {
+        int id = sqlite3_column_int(stmt, 0);
+        const char* name = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1));
+        const char* passwd = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 2)); // chagne type
+        cout << "ID: " << id << ", LOGIN: " << name << ", PASSWORD: " << passwd << endl;
+    }
+    sqlite3_finalize(stmt);
+    */
     sqlite3_close(db);
+
+    return 0;
 }
